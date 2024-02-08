9e2ec04211125ccf9211cdb778ecc65e0e5e484f
zoniak <zoniakzoak@gmail.com>
2024-02-08 02:28:41 +0100

    mod css san valentin

--- SanValentin/script.js
+++ SanValentin/script.js
@@ -20,7 +20,7 @@ document.getElementById("siBtn").addEventListener("click", function () {
 
   // Mostrar el mensaje específico
   document.getElementById("messageContainer").style.display = "block";
-  document.getElementById("messageContainer").innerHTML = "¡aaaaAAAaaaa snwajdn!";
+  document.getElementById("messageContainer").innerHTML = "¡aaaaAAAaaaa que genial te voy a dar comidita super rica!";
 
   // Mostrar otro gif después de 3 segundos
   setTimeout(function () {
